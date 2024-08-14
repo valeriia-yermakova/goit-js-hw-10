50e706d3c19e588f8985c3db2928747a4307e544
valeriia-yermakova <164086272+valeriia-yermakova@users.noreply.github.com>
2024-08-14 17:13:39 +0000

    Deploying to gh-pages from @ valeriia-yermakova/goit-js-hw-10@c41d1b6520f3b38a4cd5812063abe875240efe6f 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{i as r}from"./assets/vendor-77e16229.js";document.querySelector(".form").addEventListener("submit",function(t){t.preventDefault();const s=Number(t.target.elements.delay.value),i=t.target.elements.state.value;new Promise((e,m)=>{setTimeout(()=>{i==="fulfilled"?e(s):m(s)},s)}).then(e=>{r.success({title:"✅ Success",message:`Fulfilled promise in ${e}ms`})}).catch(e=>{r.error({title:"❌ Error",message:`Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=commonHelpers2.js.map
